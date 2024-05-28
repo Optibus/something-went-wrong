8c2f635041354867d6bc633609ce2965c199c31b
Wisam112 <wisam.armaly@optibus.com>
2024-05-28 14:08:45 +0300

    Add flask endpoint ping, invoke

--- main.py
+++ main.py
@@ -43,38 +43,41 @@ load_dotenv()
 # Anthropic
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 
+llm = ChatAnthropic(
+    anthropic_api_key=ANTHROPIC_API_KEY,
+    temperature=0,
+    model="claude-3-opus-20240229",
+    max_tokens=4096,
+)
+
+llm_haiku = ChatAnthropic(
+    anthropic_api_key=ANTHROPIC_API_KEY,
+    temperature=0,
+    model="claude-3-haiku-20240307",
+    max_tokens=4096,
+)
+
+agent = DareAgent(
+    name="Agent - Output Agent",
+    llm=llm_haiku,
+    vision="To provide accurate and helpful information to the user, while ensuring that the information is relevant to the user's query",
+    mission="To provide a solution to the user's problem",
+)
+
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
-if __name__ == "__main__":
-    llm = ChatAnthropic(
-        anthropic_api_key=ANTHROPIC_API_KEY,
-        temperature=0,
-        # model="claude-3-haiku-20240307",
-        model="claude-3-opus-20240229",
-        max_tokens=4096,
-    )
 
-    llm_haiku = ChatAnthropic(
-        anthropic_api_key=ANTHROPIC_API_KEY,
-        temperature=0,
-        model="claude-3-haiku-20240307",
-        max_tokens=4096,
-    )
+@app.route("/ping")
+def ping():
+    return "pong"
 
-    agent = DareAgent(
-        name="Agent - Output Agent",
-        llm=llm_haiku,
-        vision="To provide accurate and helpful information to the user, while ensuring that the information is relevant to the user's query",
-        mission="To provide a solution to the user's problem",
-    )
 
-    print("I AM HERE HERE")
-    res = agent.invoke(
-        input="What is the error",
-        context="Something went wrong when importing the module",
-    )
-
-    print("FINAL FINAL OUTPUT")
-    print("====================================")
-    print(res)
+@app.route("/invoke", methods=["POST"])
+def invoke():
+    data = request.json
+    print("DATA")
+    print(data)
+    input = data["input"]
+    res = agent.invoke(input=input, context="")
+    return res
