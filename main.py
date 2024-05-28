5f918aa260d056f109b2b89c4545b40737cbcb93
ereleventer <erel@optibus.com>
2024-05-28 14:11:18 +0300

    cleanup

--- main.py
+++ main.py
@@ -1,39 +1,8 @@
 from flask import Flask, request
 from flask_cors import CORS
 import os
-from langfuse.callback import CallbackHandler
 from dotenv import load_dotenv
-from langchain_core.tools import tool
-import requests
-import base64
 from langchain_anthropic import ChatAnthropic
-from typing import List
-from langchain_core.messages import HumanMessage, BaseMessage, SystemMessage, AIMessage
-from langgraph.graph import END, MessageGraph
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.agents import AgentExecutor
-from langchain.output_parsers.openai_functions import JsonOutputFunctionsParser
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-import operator
-from typing import Annotated, Any, Dict, List, Optional, Sequence, TypedDict
-import functools
-
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langgraph.graph import StateGraph, END
-import json
-from json import JSONDecodeError
-from langchain_core.agents import AgentAction, AgentActionMessageLog, AgentFinish
-from langchain_core.exceptions import OutputParserException
-
-
-from langchain.agents.format_scratchpad.openai_functions import (
-    format_to_openai_function_messages,
-)
-
-
-from langchain_core.outputs import ChatGeneration, Generation
-
-from langchain_core.runnables import Runnable, RunnablePassthrough
 
 from classes.dare_agent import DareAgent
 
